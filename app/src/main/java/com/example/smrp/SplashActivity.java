a4867f5d47f27c4f8a77ae7bbb89ebb1a1671cca
comse <guhan589@naver.com>
2020-03-27 15:57:39 +0900

    Ver 0.0.2 is made by Guhwan Kwon

--- app/src/main/java/com/example/smrp/SplashActivity.java
+++ app/src/main/java/com/example/smrp/SplashActivity.java
@@ -2,13 +2,23 @@ package com.example.smrp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashActivity extends AppCompatActivity {
-
+    private Handler handler = new Handler();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(),StartActivity.class);
+                startActivity(intent);
+
+            }
+        },2000);
     }
 }
