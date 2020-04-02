ae15508a4b8c84284f92a88bf09ed423bcb47959
YoonSuHyeon <96-05-03@naver.com>
2020-04-02 16:17:19 +0900

    # Recovery

--- app/src/main/java/com/example/smrp/HomeActivity.java
+++ app/src/main/java/com/example/smrp/HomeActivity.java
@@ -1,67 +1,17 @@
 package com.example.smrp;
 
 
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.core.view.GravityCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
-import com.google.android.material.navigation.NavigationView;
 
 public class HomeActivity extends AppCompatActivity {
 
-    private DrawerLayout mDrawerLayout;
-    private Context context = this;
-    private AppBarConfiguration mAppBarConfiguration;
-    Intent intent;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.appbar);
-        setSupportActionBar(toolbar);
-        /*ActionBar actionBar = getSupportActionBar(); // 앱바 엑세스
-        actionBar.setDisplayShowTitleEnabled(false); // 기존 title 지우기
-        actionBar.setDisplayHomeAsUpEnabled(true); // 뒤로가기 버튼 만들기
-        actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp); //뒤로가기 버튼 이미지 지정
-*/
-
-
-        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);//nav_view: activity_home.xml 의 네비게이션 id
-        mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.item1, R.id.item2, R.id.item3, R.id.item4, R.id.item5, R.id.item6, R.id.item7
-                , R.id.item8, R.id.item9) //nav_home, nave_gallery, nav_slideshow는 nav_menu.xml 안에 item의 id값
-                .setDrawerLayout(mDrawerLayout)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment); //nav_host_fragment: content_main.xml 의 프래그먼트 id값
-        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
-        NavigationUI.setupWithNavController(navigationView, navController);
-    }
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        return true;
-    }
-
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
-                || super.onSupportNavigateUp();
     }
 }
