4ece7da79b1c7faed3191c28b43d6465de7e8751
YoonSuHyeon <96-05-03@naver.com>
2020-04-06 17:27:54 +0900

    # TOS Activity (이용약관)  Test

--- app/src/main/java/com/example/smrp/SignUpActivity.java
+++ app/src/main/java/com/example/smrp/SignUpActivity.java
@@ -3,6 +3,7 @@ package com.example.smrp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -13,7 +14,7 @@ import android.widget.ImageView;
 public class SignUpActivity extends AppCompatActivity {
     /* 이름, 아이디, 이메일, 비밀번호, 비밀번호 확인 객치(여기서 sua는 SignUpActivity의 앞글자를 딴 것이다.) */
 
-
+    Context context;
     ImageView iv_back; //뒤로가기 이미지
     Button Btn_sua_singup; // 회원가입 버튼 객체
     @SuppressLint("WrongViewCast")
@@ -24,13 +25,16 @@ public class SignUpActivity extends AppCompatActivity {
 
 
 
-
+        context=this;
         Btn_sua_singup = findViewById(R.id.Btn_sua_signup);
 
         Btn_sua_singup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                onBackPressed();
+                Intent intent = new Intent(context,LoginActivity.class);
+                startActivity(intent);
+                finish();
+
             }
         });
 
--- app/src/main/java/com/example/smrp/ToSActivity.java
+++ app/src/main/java/com/example/smrp/ToSActivity.java
@@ -1,22 +1,30 @@
 package com.example.smrp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatCheckBox;
 
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.ImageView;
 
 public class ToSActivity extends AppCompatActivity {
     Context context; //this 객체
     Button btn_agree; // 동의 버튼 객체
     ImageView iv_back; //뒤로가기 이미지
+    CheckBox checkAll,check1,check2,check3;  //전체동의 등... 체크박스
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_to_s);
+
+        checkAll=findViewById(R.id.cb_All);
+        check1=findViewById(R.id.cb_Check1);
+        check2=findViewById(R.id.cb_Check2);
+        check3=findViewById(R.id.cb_Check3);
 
         //Init
         context=this;
@@ -24,8 +32,11 @@ public class ToSActivity extends AppCompatActivity {
         btn_agree.setOnClickListener(new View.OnClickListener() { //동의 버튼  클릭시
             @Override
             public void onClick(View v) {
+
                 Intent intent =new Intent(context,SignUpActivity.class);
                 startActivity(intent);
+                finish();
+
             }
         });
         iv_back=findViewById(R.id.iv_back);
@@ -35,6 +46,8 @@ public class ToSActivity extends AppCompatActivity {
                 onBackPressed();
             }
         });
+
+
 
 
 
