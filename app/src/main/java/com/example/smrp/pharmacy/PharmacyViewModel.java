bcc7b25f760f6ffca840a567fc239022996c11d1
KwonGuHwan <guhan589@naver.com>
2020-04-07 16:22:29 +0900

    Merge branch 'master' of C:\Users\COMSE\AndroidStudioProjects\SMRP3 with conflicts.

--- app/src/main/java/com/example/smrp/pharmacy/PharmacyViewModel.java
+++ app/src/main/java/com/example/smrp/pharmacy/PharmacyViewModel.java
@@ -211,6 +211,7 @@ public class PharmacyViewModel extends ViewModel {
     }
 
     public LiveData<String> getText() {
+        total_information.delete(0,total_information.length()-1);
         return mText;
     }
 }
