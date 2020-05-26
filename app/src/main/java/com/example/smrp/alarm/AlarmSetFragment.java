6c9d0e399b9bb2545064f4390b013d8a2a4853b8
comse <guhan589@naver.com>
2020-05-26 20:20:36 +0900

    `20.05.26 권구환`

---
+++ app/src/main/java/com/example/smrp/alarm/AlarmSetFragment.java
@@ -0,0 +1,58 @@
+package com.example.smrp.alarm;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.example.smrp.R;
+
+import java.util.ArrayList;
+
+public class AlarmSetFragment extends Fragment {
+
+    private AlarmViewModel alarmViewModel;
+    private Spinner spin_type;
+    ArrayList<String> typeList; // 식전, 식후 담는 리스트
+    ArrayAdapter<String> arrayAdapter; // 배열 어댑터
+
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        alarmViewModel =
+                ViewModelProviders.of(this).get(AlarmViewModel.class);
+        View root = inflater.inflate(R.layout.alarm_med_fragment, container, false);
+
+        spin_type = root.findViewById(R.id.spin_type);
+
+        typeList = new ArrayList<>();
+        typeList.add("식전");
+        typeList.add("식후");
+        arrayAdapter = new ArrayAdapter<>(getContext().getApplicationContext(),
+                android.R.layout.simple_spinner_dropdown_item,
+                typeList);
+        spin_type.setAdapter(arrayAdapter);
+        spin_type.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(getContext().getApplicationContext(),typeList.get(i)+"",
+                        Toast.LENGTH_SHORT).show();
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+            }
+        });
+
+
+        return root;
+    }
+
+}
