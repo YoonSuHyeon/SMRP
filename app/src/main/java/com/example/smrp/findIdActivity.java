da30801dd19ba32f9ce01f50f9b81ed1d16f4d36
comse <guhan589@naver.com>
2020-06-25 14:31:11 +0900

    `20.6.25 권구환`-3

--- app/src/main/java/com/example/smrp/findIdActivity.java
+++ app/src/main/java/com/example/smrp/findIdActivity.java
@@ -1,4 +1,5 @@
 package com.example.smrp;
+
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
@@ -24,14 +25,11 @@ public class findIdActivity extends AppCompatActivity {
     private Fragment_id fragment_id;
     private Fragment_password fragment_password;
     private FragmentTransaction transaction;
-    private static findIdActivity findIdActivity;
-    private Change_Password change_password;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_id);
-
-        findIdActivity = this;
 
         //btn_findId=findViewById(R.id.btn_findId);
         //btn_findPassword=findViewById(R.id.btn_findPassword);
@@ -53,29 +51,28 @@ public class findIdActivity extends AppCompatActivity {
         ll_fragment_password= findViewById(R.id.ll_fragment_password);
         ll_id = findViewById(R.id.ll_id);
         ll_password = findViewById(R.id.ll_password);
-        tv_id = findViewById(R.id.tv_id); // 아이디 찾기 배너
-        tv_password=findViewById(R.id.tv_password); // 비밀번호 찾기 배너
+        tv_id = findViewById(R.id.tv_id);
+        tv_password=findViewById(R.id.tv_password);
 
-        change_password = new Change_Password("","","","");
         View.OnClickListener clickListener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 transaction = fragmentManager.beginTransaction();
-                findIdActivity = new findIdActivity();
+
                 switch (v.getId()){
 
-                    case R.id.ll_fragment_id:{ //아이디찾기 내용을 표시할 레이아웃
+                    case R.id.ll_fragment_id:{
 
-                        transaction.replace(R.id.frame_layout,fragment_id).commitAllowingStateLoss();  // fragment_id => Fragment_id 클래스 객체
-                        tv_id.setTextColor(Color.parseColor("#2196F3"));                     // frame_layout : 아이디, 비밀번호 찾기의 디자인을 표실할 구역
+                        transaction.replace(R.id.frame_layout,fragment_id).commitAllowingStateLoss();
+                        tv_id.setTextColor(Color.parseColor("#2196F3"));
                         ll_id.setBackgroundColor(Color.parseColor("#2196F3"));
                         tv_password.setTextColor(Color.parseColor("#666464"));
                         ll_password.setBackgroundColor(Color.parseColor("#666464"));
                         break;
                     }
-                    case R.id.ll_fragment_password:{ //비밀번호찾기 내용을 표실할 레이아웃
-                                                                                                             // fragment_password => fragment_password 클래스 객체
-                        transaction.replace(R.id.frame_layout,fragment_password).commitAllowingStateLoss();  // frame_layout : 아이디, 비밀번호 찾기의 디자인을 표실할 구역
+                    case R.id.ll_fragment_password:{
+
+                        transaction.replace(R.id.frame_layout,fragment_password).commitAllowingStateLoss();
                         tv_id.setTextColor(Color.parseColor("#666464"));
                         ll_id.setBackgroundColor(Color.parseColor("#666464"));
                         tv_password.setTextColor(Color.parseColor("#2196F3"));
@@ -86,8 +83,8 @@ public class findIdActivity extends AppCompatActivity {
                 }
             }
         };
-        ll_fragment_id.setOnClickListener(clickListener); //아이디 찾기
-        ll_fragment_password.setOnClickListener(clickListener); //비밀번호찾기
+        ll_fragment_id.setOnClickListener(clickListener);
+        ll_fragment_password.setOnClickListener(clickListener);
 
 
         fragmentManager = getSupportFragmentManager();
@@ -104,11 +101,4 @@ public class findIdActivity extends AppCompatActivity {
    /*public void onClick(View view){
 
     }*/
-
-   public static  findIdActivity getInstance(){
-       return findIdActivity;
-   }
-   public Change_Password getChang_Password(){
-       return change_password;
-   }
 }
